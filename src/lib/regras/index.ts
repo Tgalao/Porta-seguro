970e4065828041115903efaa5bd8acffc66cebcd
Gtoms-cell <tseabra2009@gmail.com>
2026-09-02 09:20:05 +0100

    Adiciona indice da pasta regras/ (ponto unico de acesso)

---
+++ src/lib/regras/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Ponto único de acesso à lógica de decisão da Fase 3.
+ */
+
+export { decidirSaida, type AlunoParaDecisaoSaida, type DecisaoSaida } from "./decidirSaida";
+export {
+  decidirEntrada,
+  type AlunoParaDecisaoEntrada,
+  type DecisaoEntrada,
+} from "./decidirEntrada";
+export {
+  validarTokenQR,
+  type DadosTokenQR,
+  type MotivoTokenInvalido,
+  type ResultadoValidacaoQR,
+} from "./validarTokenQR";
+export { encontrarBlocoADecorrer } from "./horarios";
